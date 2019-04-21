a878c3006913c9876aaae698ef5052d8b3331112
Cara Wang <wlin@redhat.com>
2019-04-21 20:08:21 +0800

    Chapter 1-5: 变量

---
+++ variables.groovy
@@ -0,0 +1,10 @@
+class Example {
+  static void main(String[] args){
+    int x = 5 ;
+    def int y = 6 ;
+    def z = 7 ;
+    println("The value of x is " + x);
+    println(y);
+    println(z);
+  }
+}
